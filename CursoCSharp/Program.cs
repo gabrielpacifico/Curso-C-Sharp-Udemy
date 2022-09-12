49fc3e002bef525b8eb87532557d96214cd4b897
Gabriel Pacífico <vgabrielap17@gmail.com>
2022-09-12 17:00:46 -0300

    Changes 12/09

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -5,6 +5,7 @@ using CursoCSharp.Fundamentos;
 using CursoCSharp.EstruturasDeControle;
 using CursoCSharp.ClassesEMetodos;
 using CursoCSharp.Colecoes;
+using CursoCSharp.OO;
 
 namespace CursoCSharp {
     class Program {
@@ -64,6 +65,8 @@ namespace CursoCSharp {
                 {"Igualdade - Coleções ", Igualdade.Executar},
                 {"Stack - Coleções ", ColecoesStack.Executar},
                 {"Dicionario - Coleções ", ColecoesDicionario.Executar},
+                // OO
+                {"Herança - Orientação à objetos ", Heranca.Executar},
 
             });
 
